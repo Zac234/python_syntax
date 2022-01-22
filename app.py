6863acd37a9375f828fed48e250f69c7f5f54bac
zac234 <zacbenincasa@gamil.com>
2022-01-22 14:35:08 -0700

    hopefully it works

---
+++ app.py
@@ -0,0 +1,12 @@
+from lib2to3.pgen2.token import NUMBER
+
+
+NUMBER(3,7,9,15,25,30,66,34,12,51,27,25,60,22,10)
+    
+
+if(NUMBER%3):
+    print ("fizz")
+elif(NUMBER%5):
+    print ("buzz")
+elif (NUMBER%3,5):
+    print("fizzbuzz")
